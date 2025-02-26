d862be89962687abdefc8a9bc32c4a5f7d928b11
anupamhaldkar <anupam.haldkar@outlook.com>
2025-02-26 17:56:33 +0530

    Implement product management features with JPA integration and REST API

---
+++ catalog-service/src/main/java/com/ahdev/bookstore/catalog/domain/ProductService.java
@@ -0,0 +1,38 @@
+package com.ahdev.bookstore.catalog.domain;
+
+import com.ahdev.bookstore.catalog.ApplicationProperties;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class ProductService {
+
+    private final ProductRepository productRepository;
+    private final ApplicationProperties applicationProperties;
+
+    public ProductService(ProductRepository productRepository, ApplicationProperties applicationProperties) {
+        this.productRepository = productRepository;
+        this.applicationProperties = applicationProperties;
+    }
+
+    public PagedResult<Product> getProducts(int pageNo) {
+        Sort sort = Sort.by("name").ascending();
+        pageNo = pageNo <= 1 ? 0 : pageNo - 1;
+        PageRequest pageable = PageRequest.of(pageNo, applicationProperties.pageSize(), sort);
+        Page<Product> products = productRepository.findAll(pageable).map(ProductMapper::toProduct);
+
+        return new PagedResult<>(
+                products.getContent(),
+                products.getTotalElements(),
+                products.getNumber() + 1,
+                products.getTotalPages(),
+                products.isFirst(),
+                products.isLast(),
+                products.hasNext(),
+                products.hasPrevious());
+    }
+}
