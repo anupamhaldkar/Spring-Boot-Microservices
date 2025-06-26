925a678f9d9c3eb25914345e2ca941c210c51fe8
anupamhaldkar <anupam.haldkar@outlook.com>
2025-06-26 18:37:41 +0530

    feat: add unit and integration tests for order creation functionality

---
+++ order-service/src/test/java/com/ahdev/bookstore/orders/web/controllers/OrderControllerTests.java
@@ -0,0 +1,68 @@
+package com.ahdev.bookstore.orders.web.controllers;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.notNullValue;
+
+import com.ahdev.bookstore.orders.AbstractIT;
+import com.ahdev.bookstore.orders.testdata.TestDataFactory;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+
+class OrderControllerTests extends AbstractIT {
+
+    @Nested
+    class CreateOrderTests {
+        @Test
+        void shouldCreateOrderSuccessfully() {
+            // mockGetProductByCode("P100", "Product 1", new BigDecimal("25.50"));
+            var payload =
+                    """
+                        {
+                            "customer" : {
+                                "name": "Siva",
+                                "email": "siva@gmail.com",
+                                "phone": "999999999"
+                            },
+                            "deliveryAddress" : {
+                                "addressLine1": "HNO 123",
+                                "addressLine2": "Kukatpally",
+                                "city": "Hyderabad",
+                                "state": "Telangana",
+                                "zipCode": "500072",
+                                "country": "India"
+                            },
+                            "items": [
+                                {
+                                    "code": "P100",
+                                    "name": "Product 1",
+                                    "price": 25.50,
+                                    "quantity": 1
+                                }
+                            ]
+                        }
+                    """;
+            given().contentType(ContentType.JSON)
+                    // .header("Authorization", "Bearer " + getToken())
+                    .body(payload)
+                    .when()
+                    .post("/api/orders")
+                    .then()
+                    .statusCode(HttpStatus.CREATED.value())
+                    .body("orderNumber", notNullValue());
+        }
+
+        @Test
+        void shouldReturnBadRequestWhenMandatoryDataIsMissing() {
+            var payload = TestDataFactory.createOrderRequestWithInvalidCustomer();
+            given().contentType(ContentType.JSON)
+                    // .header("Authorization", "Bearer " + getToken())
+                    .body(payload)
+                    .when()
+                    .post("/api/orders")
+                    .then()
+                    .statusCode(HttpStatus.BAD_REQUEST.value());
+        }
+    }
+}
