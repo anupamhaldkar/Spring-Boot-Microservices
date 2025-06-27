50f8a77b60e975c11036e260a901f5a5792bb51e
anupamhaldkar <anupam.haldkar@outlook.com>
2025-06-27 23:38:20 +0530

    feat: add CatalogServiceClientConfig for RestClient configuration

---
+++ order-service/src/main/java/com/ahdev/bookstore/orders/clients/config/CatalogServiceClientConfig.java
@@ -0,0 +1,25 @@
+package com.ahdev.bookstore.orders.clients.config;
+
+import com.ahdev.bookstore.orders.ApplicationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+
+@Configuration
+class CatalogServiceClientConfig {
+    @Bean
+    RestClient restClient(
+            // RestClient.Builder builder,
+            ApplicationProperties properties) {
+        // ClientHttpRequestFactory requestFactory = ClientHttpRequestFactoryBuilder.simple()
+        //         .withCustomizer(customizer -> {
+        //             customizer.setConnectTimeout(Duration.ofSeconds(5));
+        //             customizer.setReadTimeout(Duration.ofSeconds(5));
+        //         })
+        //         .build();
+        // return builder.baseUrl(properties.catalogServiceUrl())
+        //         .requestFactory(requestFactory)
+        //         .build();
+        return RestClient.builder().baseUrl(properties.catalogServiceUrl()).build();
+    }
+}
