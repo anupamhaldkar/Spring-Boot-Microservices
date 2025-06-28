ca1bb17c0440d7f9766d649d4879539cbdb21931
anupamhaldkar <anupam.haldkar@outlook.com>
2025-06-28 17:11:30 +0530

    feat: update CatalogServiceClientConfig to include ApplicationProperties in RestClient bean

--- order-service/src/main/java/com/ahdev/bookstore/orders/clients/config/CatalogServiceClientConfig.java
+++ order-service/src/main/java/com/ahdev/bookstore/orders/clients/config/CatalogServiceClientConfig.java
@@ -10,6 +10,7 @@ class CatalogServiceClientConfig {
     @Bean
     RestClient restClient(
             // RestClient.Builder builder,
+           
             ApplicationProperties properties) {
         // ClientHttpRequestFactory requestFactory = ClientHttpRequestFactoryBuilder.simple()
         //         .withCustomizer(customizer -> {
