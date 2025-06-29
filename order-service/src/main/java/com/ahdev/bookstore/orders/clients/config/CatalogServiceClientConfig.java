51d53a40cd4b1ee824abc50ca8bdd9ab54ae1498
anupamhaldkar <anupam.haldkar@outlook.com>
2025-06-29 21:15:39 +0530

    feat: add Product record and update CatalogServiceClientConfig to include ApplicationProperties

--- order-service/src/main/java/com/ahdev/bookstore/orders/clients/config/CatalogServiceClientConfig.java
+++ order-service/src/main/java/com/ahdev/bookstore/orders/clients/config/CatalogServiceClientConfig.java
@@ -10,7 +10,7 @@ class CatalogServiceClientConfig {
     @Bean
     RestClient restClient(
             // RestClient.Builder builder,
-           
+
             ApplicationProperties properties) {
         // ClientHttpRequestFactory requestFactory = ClientHttpRequestFactoryBuilder.simple()
         //         .withCustomizer(customizer -> {
